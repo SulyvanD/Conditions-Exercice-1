f86b0ff36afda9a98f2f60fc9ebeb455d43ca805
SulyvanD <sulyvan.debraize@net-c.com>
2018-12-18 11:53:31 +0100

    Exercice completé

---
+++ Conditions - Exercice 1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conditions___Exercice_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool rainy = false;
+
+            if (rainy) {
+                Console.WriteLine("Bonjour, il ne pleut pas aujourd’hui, bonne journée.");
+            } else {
+                Console.WriteLine("Bonjour, il pleut aujourd’hui, vous devriez prendre votre parapluie.");
+            }
+        }
+    }
+}
